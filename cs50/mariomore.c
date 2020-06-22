37bc9d27e370059d99ff3f33d6e2ed42d6559d43
Vinicius <viniciusnalm@gmail.com>
2020-06-22 16:26:28 -0300

    mariomore

---
+++ cs50/mariomore.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "cs50.h"
+
+int main(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Height: ");
+    } while (n < 1 || n > 8);
+
+    for (int row = 0; row < n; row++)
+    {
+        for (int i = n - 1; i > row; i--)
+        {
+            printf(" ");
+        }
+
+        for (int column = 0; column <= row; column++)
+        {
+
+            printf("#");
+        }
+
+        printf("  ");
+        printf("#");
+
+        for (int x = 0; x < row; x++)
+        {
+            printf("#");
+            
+        }
+
+        printf("\n");
+    }
+}
---
+++ overallc/forca.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char palavra[20];
+
+    sprintf(palavra, "centenario");
+    
+
+    printf("%s\n", palavra);
+   
+    
+}
